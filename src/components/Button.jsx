d81d056b50522e8e0bc78441b129b9828336d46d
Kartik Talati <80997418+Kartik1006@users.noreply.github.com>
2025-03-29 20:06:11 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/Button.css"; // Ensure you create this CSS file
+import "../styles/button.css"; // Ensure you create this CSS file
 
 const Button = ({ text, onClick }) => {
   return (
